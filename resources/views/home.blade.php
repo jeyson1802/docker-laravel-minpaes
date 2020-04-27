ab69a3046c3efb6b135510583e9a48ebacd746e6
Jeyson Junior Ventura Aguilar <jeysonv.sis@gmail.com>
2020-04-27 10:06:07 -0500

    pagina en construccion

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,193 +1,106 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
 
-@section('title', 'Inicio')
+  <head>
 
-@section('content')
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <section id="banner">
-        <div class="banner-item">
-        </div><!-- Banner item end -->
-    </section><!-- Section banner end -->
+    <!-- Main CSS -->
+    <link href="dist/css/main.css" rel="stylesheet">
 
-    <section id="ts-intro" class="ts-intro">
+    <!-- Your page title -->
+    <title>Pr&oacute;ximamente | MINPAES</title>
+
+    <!-- Place Custom Favicon here -->
+    <link rel="shortcut icon" href="images/favicon.png" type="image/png">
+
+
+  </head>
+
+  <body class="coming-soon-page">
+
+    <div class="page-loader">
+      <div class="loader"><img src="images/logo-sm.svg" alt=""></div>
+    </div>
+
+    <!-- =================== SITE HEADER BEGINS ============================= -->
+
+    <header class="header fixed">
+
+      <div class="container">
+
+        <nav class="header__nav bottom-nav">
+
+          <div class="header__logo brand--logo">
+            <a href="index.html"><img src="images/logo-dark.png" alt="Greater Love Church"></a>
+          </div>
+
+        </nav><!-- .header__nav ends -->
+
+      </div><!-- .container ends -->
+
+    </header><!-- .header ends -->
+
+    <!-- =================== SITE HEADER ENDS ============================= -->
+
+
+
+    <!-- =================== MAIN SECTION BEGINS ============================= -->
+
+    <main>
+
+      <!-- COMING SOON SECTION STARTS -->
+      <section class="coming-soon display-flex-column justify-content-center default-section-spacing">
+
+        <div class="coming-soon__cover"></div>
+
         <div class="container">
 
-            <div class="row text-center">
-                <h2 class="section-title">Cruzada de Avivamiento</h2>
-                <p class="section-sub-title">Profeta Dr. David Owuor</p>
-            </div><!--/ Title row end -->
+          <div class="row justify-content-end">
 
-            <div class="row">
-                <div class="col-xs-12 col-sm-12 col-md-6">
-                    <div class="intro-video">
-                        <img class="img-responsive" src="images/intro-video.jpg" alt="" />
-                        <a class="popup" href="https://www.youtube.com/embed/mw6dcFRVy40?autoplay=1&amp;loop=1">
-                            <div class="video-icon">
-                                <i class="fa fa-play"></i>
-                            </div>
-                        </a>
-                    </div>
-                </div><!-- Col end -->
+            <div class="flex-lg-6">
 
-                <div class="col-xs-12 col-sm-12 col-md-6">
-                    <div class="featured-tab">
-                        <ul class="nav nav-tabs">
-                            <li class="active">
-                                <a class="animated fadeIn" href="#tab_a" data-toggle="tab">
-                                    Arrepentimiento y Avivamiento
-                                </a>
-                            </li>                        
-                        </ul>
+              <div class="coming-soon__content text-center">
 
-                        <div class="tab-content">
-                            <div class="tab-pane active animated fadeInRight" id="tab_a">
-                                <h3 class="tab-content-title">Arrepentimiento y Avivamiento</h3>
-                                <p class="sppb-cta-text"> El Señor nos invita a arrepentirnos y volver a sus caminos, nos invita a ser llenos del Espiritu Santo de Dios.
-                                </p>
-                                <blockquote>	El Espíritu del Señor está sobre mí, Por cuanto me ha ungido para dar buenas nuevas a los pobres; Me ha enviado a sanar a los quebrantados de corazón; A pregonar libertad a los cautivos, Y vista a los ciegos; A poner en libertad a los oprimidos - Lucas 4:18</blockquote>
-                            </div><!-- Tab pane 1 end -->                            
-                        </div><!-- tab content -->
-                    </div><!-- Featured tab end -->
-                </div><!-- Col end -->
+                <h1>Pr&oacute;ximamente</h1>
 
-            </div><!-- Content Row end -->
-        </div><!-- Container end -->
-    </section><!-- Intro area end -->
+                <p class="leading">¡Actualmente estamos trabajando en algo incre&iacute;ble que te encantar&aacute;!</p>
 
-    <section id="facts" class="facts-area bg-overlay">
-        <div class="container">
-            <div class="row">
-                <div class="facts-wrapper">
-                    <div class="col-sm-3 ts-facts">
-						<span class="ts-facts-icon">
-							<i class="fa fa-users"></i>
-						</span>
-                        <div class="ts-facts-content">
-                            <h2 class="ts-facts-num"><span class="counterUp">20</span></h2>
-                            <h3 class="ts-facts-title">Pastores</h3>
-                        </div>
-                    </div><!-- Col 1 end -->
+              </div><!-- .coming-soon__content ends -->
 
-                    <div class="col-sm-3 ts-facts">
-                        <div class="ts-facts-icon">
-                            <i class="fa fa-microphone"></i>
-                        </div>
-                        <div class="ts-facts-content">
-                            <h2 class="ts-facts-num"><span class="counterUp">1</span></h2>
-                            <h3 class="ts-facts-title">Conferencistas</h3>
-                        </div>
-                    </div><!-- Col 2 end -->
+            </div><!-- .flex-* ends -->
 
-                    <div class="col-sm-3 ts-facts">
-                        <div class="ts-facts-icon">
-                            <i class="fa fa-calendar"></i>
-                        </div>
-                        <div class="ts-facts-content">
-                            <h2 class="ts-facts-num"><span class="counterUp">4</span></h2>
-                            <h3 class="ts-facts-title">Sesiones</h3>
-                        </div>
+          </div><!-- .row ends -->
 
-                    </div><!-- Col 3 end -->
+        </div><!-- .container ends -->
 
-                    <div class="col-sm-3 ts-facts">
-                        <div class="ts-facts-icon">
-                            <i class="fa fa-globe"></i>
-                        </div>
-                        <div class="ts-facts-content">
-                            <h2 class="ts-facts-num"><span class="counterUp">15</span></h2>
-                            <h3 class="ts-facts-title">Países</h3>
-                        </div>
+      </section><!-- .coming-soon ends -->
+      <!-- COMING SOON SECTION ENDS -->
 
-                    </div><!-- Col 4 end -->
-                </div><!-- Facts end -->
+    </main><!-- main ends -->
 
-            </div><!--/ Content row end -->
-        </div><!--/ Container end -->
-    </section><!-- Facts end -->
-
-    @include('programmersection')
-
-    <section id="ts-newsletter" class="ts-newsletter">
-        <div class="container">
-            <div class="row">
-                <div class="newsletter-form clearfix">
-                    <h3>Suscríbete para noticias</h3>
-                    <form action="/sendsuscription" method="post">
-                        {{ csrf_field() }}
-                        <div class="form-group">
-                            <input type="email" name="correo" id="correo" class="form-control form-control-lg" placeholder="E-mail aquí">
-                            <button class="btn btn-primary">Suscribirse</button>
-                        </div>
-                    </form>
-                </div>
-            </div><!-- Row end -->
-        </div><!--/ Container end -->
-    </section><!--/ Newsletter end -->
-
-    <section id="ts-venue" class="ts-venue">
-        <div class="container">
-            <div class="row text-center">
-                <h2 class="section-title">Mayor Información</h2>
-                <p class="section-sub-title">Donde Ubicarnos</p>
-            </div><!--/ Title row end -->
-
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="venu-info-content box1">
-                        <span class="venu-info-icon"><i class="fa fa-map-marker"></i></span>
-                        <h3 class="venu-info-title">Lugar</h3>
-                        <p class="venu-info-text">La cruzada de avivamiento se llevará a cabo en <b> Explanada 200 Millas - Ovalo Las Palomas - Frente al Parque Huascar - V.E.S - Lima - Perú </b>, para mayor información contáctenos..</p>
-                        <a href="{{ url('contactodrowuorperu') }}" class="venu-info-link">Leer Más <i class="fa fa-angle-right"></i></a>
-                    </div><!-- Venu content end -->
-                </div><!-- Col 1 end -->
-
-            </div><!-- Content row end -->
-        </div><!-- Container end -->
-    </section><!-- Venue end -->
-
-    <div id="map" class="map"></div> <!-- Google Map -->
-@endsection
-
-@push('scripts')
-
-    <!-- Google Map -->
-    <script type="text/javascript" src="//maps.googleapis.com/maps/api/js?key=AIzaSyD8nTg4zwDHEZYRnbNyUQnI78kshb31RoI"></script>
-    <!-- Doc https://developers.google.com/maps/documentation/javascript/get-api-key -->
-    <!-- For latitude and longitude use http://www.latlong.net/ -->
-
-    <script type="text/javascript" src="js/gmap3.min.js"></script>
-
-    <script type="text/javascript">
-
-        /* Event map */
-
-        var eventmap = {lat: -12.229033, lng: -76.928103};
-
-        $('.map')
-            .gmap3({
-                zoom: 16,
-                center: eventmap,
-                mapTypeId : google.maps.MapTypeId.ROADMAP,
-                scrollwheel: false
-            })
-
-            .marker({
-                position: eventmap
-            })
-
-            .infowindow({
-                position: eventmap,
-                content: "Explanada 200 Millas - V.E.S"
-            })
-
-            .then(function (infowindow) {
-                var map = this.get(0);
-                var marker = this.get(1);
-                marker.addListener('click', function() {
-                    infowindow.open(map, marker);
-                });
-            });
+    <!-- =================== MAIN SECTION ENDS ============================= -->
 
 
-    </script>
-@endpush
+    <!-- JQUERY -->
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+    <!-- loading in JQuery locally if CDN failed -->
+    <script>window.jQuery || document.write('<script src="js/jquery.min.js">\x3C/script>')</script>
+    <!-- Owl Carousel script -->
+    <script src="js/plugins/owl.carousel.js"></script>
+    <!-- JQuery Validator script -->
+    <script src="js/plugins/jquery.validate.js"></script>
+    <!-- JQuery Credit Card script -->
+    <script src="js/plugins/jquery.card.js"></script>
+    <!-- Animate On Scroll script -->
+    <script src="js/plugins/aos.js"></script>
+    <!-- Lightbox script -->
+    <script src="js/plugins/lightbox.min.js"></script>
+    <!-- Main script -->
+    <script src="dist/js/main.min.js"></script>
+
+  </body>
+
+</html>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,50 +2,18 @@
 <html lang="en">
 <head>
 
-    <!-- Basic Page Needs
-    ================================================== -->
     <meta charset="utf-8">
-
-
-
-    
-    <!-- Mobile Specific Metas
-    ================================================== -->
-
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <!-- Main CSS -->
+    <link href="dist/css/main.css" rel="stylesheet">
 
-    <!-- Site Title -->
+    <!-- Your page title -->
     <title>@yield('title')</title>
 
-    <!--Favicon-->
-    <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
-    <link rel="icon" href="images/favicon.ico" type="image/x-icon">
-
-    <!-- CSS
-    ================================================== -->
-
-    <!-- Bootstrap -->
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <!-- Template styles-->
-    <link rel="stylesheet" href="css/style.css">
-    <!-- Responsive styles-->
-    <link rel="stylesheet" href="css/responsive.css">
-    <!-- FontAwesome -->
-    <link rel="stylesheet" href="css/font-awesome.min.css">
-    <!-- Animation -->
-    <link rel="stylesheet" href="css/animate.css">
-    <!-- Colorbox -->
-    <link rel="stylesheet" href="css/colorbox.css">
-
-    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
+    <!-- Place Custom Favicon here -->
+    <link rel="shortcut icon" href="images/favicon.png" type="image/png">
 
 </head>
 
