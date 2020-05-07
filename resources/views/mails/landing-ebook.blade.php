1f0d8dce95894174165ad54f17aaeb95d69871ba
Jeyson Junior Ventura Aguilar <jeysonv.sis@gmail.com>
2020-05-07 17:42:09 -0500

    landingpage ebook

--- resources/views/mails/landing-ebook.blade.php
+++ resources/views/mails/landing-ebook.blade.php
@@ -7,12 +7,16 @@
 </head>
 <body>
     <p>Hola {{ $landing->nombres }}</p>
-    <p>Gracias por su interés en el nuevo libro "El Poder escondido de la Iglesia", esperamos sea de bendición
-       para su vida. Adjuntamos el libro para que pueda ser accedido desde cualquier dispositivo. Shalom.
+    <p>
+       Gracias por su interés en el nuevo libro "El Poder escondido de la Iglesia", esperamos sea de bendición
+       para su vida. 
+    </p>
+    <p>
+        Adjuntamos el libro para que pueda ser accedido desde cualquier dispositivo.
     </p>
     <br>
     <img src="{{ $message->embed(public_path() . '/images/background-ebook.jpg') }}" />
     <br>
-    <p>Bendecido día.</p>
+    <p>Bendecido día. Shalom. </p>
 </body>
 </html>
